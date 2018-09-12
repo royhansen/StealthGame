8f7fefaf69abd7184ec3f5ac9f597a1e567d7b40
Roy Hansen <royhansen116@gmail.com>
2018-09-11 22:24:24 -0600

    Section 3 Lesson 21 Complete Post Mission Camera

--- StealthGame/Source/FPSGame/Private/FPSExtractionZone.cpp
+++ StealthGame/Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -3,6 +3,10 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -25,8 +29,23 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Overlapped with extraction zone!"));
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
 
+	if (MyPawn == nullptr) {
+		return;
+	}
+
+	if (MyPawn->bIsCarryingObjective) {
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM) {
+			GM->CompleteMission(MyPawn);
+		}
+	}
+	else {
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Overlapped with extraction zone!"));
 }
 
 
